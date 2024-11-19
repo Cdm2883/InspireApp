a0cfb53a4577347e99184e6877ea3aa63c1f0ab8
Cdm2883 <Cdm2883@hotmail.com>
2024-11-20 01:25:41 +0800

    test: KVStorage structural storaging test

--- dataSynchronizer/src/jvmTest/kotlin/vip/cdms/inspire/storage/data/KVStorageTest.kt
+++ dataSynchronizer/src/jvmTest/kotlin/vip/cdms/inspire/storage/data/KVStorageTest.kt
@@ -42,6 +42,10 @@ class KVStorageTest {
         TestStorage["3"] = TestBundle(test = true)
         assert(TestStorage.get<TestBundle>("3")!!.name == "Default")
         assert(TestStorage.get<TestBundle>("3")!!.test)
+        TestStorage["3"] = null
+        val mustNull: String? = TestStorage["3"]
+        assert(mustNull == null)
+        assert(TestStorage.get<TestBundle>("3") == null)
     }
 
     @Test
@@ -64,6 +68,53 @@ class KVStorageTest {
         onNodeWithTag("text").assertTextEquals("null nonNull")
         onNodeWithTag("button").performClick()
         onNodeWithTag("text").assertTextEquals("filled received")
+    }
+
+    @Test
+    fun structuralKeyOfTest() {
+        val localUser = object : KVStorage.KeyOf("Settings", "User"),
+            StorageProvider by TestStorage {
+            var name = keyOf<String>("nickname") { "Unnamed" }
+            var age by keyOf<Int>("age")
+            val accounts = object : Nesting("account") {
+                var github by keyOf("github") { "ghost" }
+                var bilibili by keyOf<String>("bilibili")
+            }
+        }
+
+        assert(localUser.name.value == "Unnamed")
+        localUser.name.value = "Kawulf"
+        assert(localUser.name.value == "Kawulf")
+        localUser.name.clear()
+        assert(localUser.name.value == "Unnamed")
+
+        assert(localUser.age == null)
+        localUser.age = 15
+        assert(localUser.age == 15)
+        assert(TestStorage.map[KVStorage.resolve(*localUser.keys, "age")] == StorageSerialization.encode(Int::class, 15))
+
+        assert(localUser.accounts.github == "ghost")
+        localUser.accounts.github = "Cdm2883"
+        assert(localUser.accounts.github == "Cdm2883")
+        assert(TestStorage.map[localUser.accounts.resolve("github")] == StorageSerialization.encode(String::class, "Cdm2883"))
+
+        assert(localUser.accounts.bilibili == null)
+        localUser.accounts.bilibili = "Cdm2883"
+        assert(localUser.accounts.bilibili == "Cdm2883")
+        assert(TestStorage.map[KVStorage.resolve("Settings", "User", "account", "bilibili")] == StorageSerialization.encode(String::class, "Cdm2883"))
+
+        var outerProperty by localUser.keyOf<Boolean>("outer")
+        outerProperty = true
+        assert(outerProperty!!)
+        assert(TestStorage.map[localUser.resolve("outer")] == StorageSerialization.encode(Boolean::class, true))
+
+        val extendProperties = object : KVStorage.KeyOf(localUser), StorageProvider by localUser {
+            var favoriteLanguage by keyOf<String>("language") { "TS" }
+        }
+        assert(extendProperties.favoriteLanguage == "TS")
+        extendProperties.favoriteLanguage = "Kotlin"
+        assert(extendProperties.favoriteLanguage == "Kotlin")
+        assert(TestStorage.map[localUser.resolve("language")] == StorageSerialization.encode(String::class, "Kotlin"))
     }
 
 }
