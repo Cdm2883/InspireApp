465dfa8b30aea5db54844c28c24081dac955fbd3
Cdm2883 <Cdm2883@hotmail.com>
2024-11-19 21:45:06 +0800

    feat: StorageRoot web implementation

--- dataSynchronizer/src/webMain/kotlin/vip/cdms/inspire/storage/data/StorageRoot.web.kt
+++ dataSynchronizer/src/webMain/kotlin/vip/cdms/inspire/storage/data/StorageRoot.web.kt
@@ -1,13 +1,9 @@
 package vip.cdms.inspire.storage.data
 
+import web.localStorage
+
 actual object StorageRoot : KVStorage() {
-    actual override fun setValue0(key: String, value: String?) {
-        TODO("Not yet implemented")
-    }
-    actual override fun getValue0(key: String): String? {
-        TODO("Not yet implemented")
-    }
-    actual override fun clear() {
-        TODO("Not yet implemented")
-    }
+    actual override fun setValue0(key: String, value: String?) = value?.let { localStorage.setItem(key, it) } ?: localStorage.removeItem(key)
+    actual override fun getValue0(key: String) = localStorage.getItem(key)
+    actual override fun clear() = localStorage.clear()
 }
