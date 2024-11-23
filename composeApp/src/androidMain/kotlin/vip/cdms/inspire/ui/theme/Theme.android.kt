d0a99b9628501981670d2563d510a36155666eb7
Cdm2883 <Cdm2883@hotmail.com>
2024-11-24 00:19:08 +0800

    feat(android): get dynamicColor config from theme settings

--- composeApp/src/androidMain/kotlin/vip/cdms/inspire/ui/theme/Theme.android.kt
+++ composeApp/src/androidMain/kotlin/vip/cdms/inspire/ui/theme/Theme.android.kt
@@ -6,10 +6,14 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
+
+var AppTheme.dynamicColor by AppTheme.keyOf("dynamicColor") { true }
 
 @Composable
 actual fun AppTheme(
@@ -17,11 +21,9 @@ actual fun AppTheme(
     contrast: Contrast,
     content: @Composable () -> Unit
 ) {
-    val dynamicColor = true  // TODO: get from settings
-
     val (defaultColorScheme, extendedColorScheme) = getDefaultColorSchemes(darkTheme, contrast)
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        AppTheme.dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
