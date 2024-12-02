b1c203efb090fad478d8bdec40b6e31385369c28
Cdm2883 <Cdm2883@hotmail.com>
2024-12-02 22:42:57 +0800

    feat: extract RegularJPen into a separate file
    [skip ci]

--- composeApp/whiteboard/src/jvmMain/kotlin/vip/cdms/inspire/whiteboard/main.kt
+++ composeApp/whiteboard/src/jvmMain/kotlin/vip/cdms/inspire/whiteboard/main.kt
@@ -7,59 +7,12 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import jpen.*
-import jpen.demo.StatusReport
+import jpen.PButton
+import jpen.PLevel
+import jpen.PLevelEvent
+import jpen.Pen
 import jpen.event.PenAdapter
-import jpen.event.PenListener
-import jpen.owner.AbstractPenOwner
-import jpen.owner.PenClip
-import jpen.provider.osx.CocoaProvider
-import jpen.provider.wintab.WintabProvider
-import jpen.provider.xinput.XinputProvider
-import vip.cdms.inspire.utils.JvmUtils
-import vip.cdms.inspire.whiteboard.utils.copyToJavaLibraryPath
-import vip.cdms.inspire.whiteboard.utils.tempExtractInsideJar
-import java.awt.Point
-import kotlin.concurrent.thread
-
-private class RegularPenClip : PenClip {
-    override fun evalLocationOnScreen(pointOnScreen: Point) {}
-    override operator fun contains(point: java.awt.geom.Point2D.Float) = true
-}
-private class RegularPenOwner : AbstractPenOwner() {
-    override fun getPenProviderConstructors() = listOf<PenProvider.Constructor>(
-//        jpen.provider.system.SystemProvider.Constructor(),  // only mouse & keyboard
-        XinputProvider.Constructor(),
-        WintabProvider.Constructor(),
-        CocoaProvider.Constructor())
-    private val penClip: RegularPenClip = RegularPenClip()
-    override fun getPenClip() = penClip
-    override fun draggingOutDisengaged() {}
-    override fun init() {
-        penManagerHandle.setPenManagerPaused(false)
-    }
-}
-private class JPenExample(penListener: PenListener) {
-    private val penOwner = RegularPenOwner()
-    private val penManager = PenManager(penOwner)
-    init {
-        println(StatusReport(penManager))
-        penManager.pen.addListener(penListener)
-    }
-}
-
-fun loadJPenNativeLibrary() = listOf(
-    "libjpen-2-4.so",
-    "libjpen-2-4-x86_64.so",
-    "jpen-2-3.dll",
-    "jpen-2-3-64.dll",
-    "libjpen-2-3.jnilib",
-).forEach { lib ->
-    runCatching {
-        val file = JvmUtils.tempExtractInsideJar("jpen/$lib")
-        JvmUtils.copyToJavaLibraryPath(file)
-    }
-}
+import vip.cdms.inspire.whiteboard.utils.RegularJPen
 
 fun main() = application {
     Window(
@@ -74,40 +27,35 @@ fun main() = application {
         BasicText(messages.joinToString("\n"))
 
         LaunchedEffect(Unit) {
+            val listener = object : PenAdapter() {
+                private var pen: Pen? = null
 
-            loadJPenNativeLibrary()
+                override fun penLevelEvent(ev: PLevelEvent?) {
+                    if (null == pen) pen = (ev ?: return).pen
+                }
 
-            thread {
-                JPenExample(object : PenAdapter() {
-                    private var pen: Pen? = null
+                override fun penTock(availableMillis: Long) {
+                    messages += "penTock availableMillis: $availableMillis"
+                    @Suppress("LocalVariableName") val _pen = pen ?: return
 
-                    override fun penLevelEvent(ev: PLevelEvent?) {
-                        if (null == pen) pen = (ev ?: return).pen
+                    messages += "KIND ${_pen.kind}"
+
+                    for (buttonType in PButton.Type.VALUES) {
+                        if (_pen.getButtonValue(buttonType)) {
+                            messages += "BUTTON_TYPE $buttonType"
+                        }
                     }
 
-                    override fun penTock(availableMillis: Long) {
-                        messages += "penTock availableMillis: $availableMillis"
-                        @Suppress("LocalVariableName") val _pen = pen ?: return
-
-                        messages += "KIND ${_pen.kind}"
-
-                        for (buttonType in PButton.Type.VALUES) {
-                            if (_pen.getButtonValue(buttonType)) {
-                                messages += "BUTTON_TYPE $buttonType"
-                            }
-                        }
-
-                        for (levelType in PLevel.Type.VALUES){
-                            val value = _pen.getLevelValue(levelType)
-                            messages += "LEVEL type: ${levelType}, value: $value"
-                        }
-
-                        // only process tocks after an event happens
-                        pen = null
+                    for (levelType in PLevel.Type.VALUES) {
+                        val value = _pen.getLevelValue(levelType)
+                        messages += "LEVEL type: ${levelType}, value: $value"
                     }
-                })
+
+                    // only process tocks after an event happens
+                    pen = null
+                }
             }
-
+            RegularJPen.addListener(listener)
         }
     }
 }
